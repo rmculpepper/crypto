e26fb01396215be34ea2c8a9cd234bacd4f5e8ba
Ryan Culpepper <ryanc@racket-lang.org>
2014-04-07 01:24:12 -0400

    add copyright notice, update comments

--- crypto-lib/crypto/private/rkt/pk-asn1.rkt
+++ crypto-lib/crypto/private/rkt/pk-asn1.rkt
@@ -1,3 +1,18 @@
+;; Copyright 2014 Ryan Culpepper
+;; 
+;; This library is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Lesser General Public License as published
+;; by the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;; 
+;; This library is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Lesser General Public License for more details.
+;; 
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 #lang racket/base
 (require asn1
          asn1/sequence)
@@ -21,7 +36,6 @@ References:
    - ASN.1 for DHParameter, dhKeyAgreement OID for AlgorithmIdentifier
  - PKCS#8: http://www.ietf.org/rfc/rfc5208.txt
    - ASN.1 for PrivateKeyInfo and EncryptedPrivateKeyInfo
-   - lots of holes
    - note: allows BER encoding, not just DER
    - https://tools.ietf.org/html/rfc5958
      - obsoletes PKCS#8 ??
