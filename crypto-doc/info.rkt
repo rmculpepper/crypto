a762cb5668149b41ad399aff4389b60bcd84bf08
Ryan Culpepper <ryanc@racket-lang.org>
2018-02-11 23:02:42 +0100

    crypto-doc: remove extra dir, update info

--- crypto-doc/info.rkt
+++ crypto-doc/info.rkt
@@ -1,7 +1,16 @@
 #lang info
 
-(define collection 'multi)
+;; pkg info
+
+(define version "1.0")
+(define collection "crypto")
 (define deps '("base"))
 (define build-deps '("racket-doc"
                      "scribble-lib"
                      "crypto-lib"))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "crypto")
+(define scribblings '(("scribblings/crypto.scrbl" (multi-page))))
