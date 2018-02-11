762e94fb76ca655001b6476e263483a997bdbb82
Ryan Culpepper <ryanc@racket-lang.org>
2018-02-11 23:05:46 +0100

    crypto-test: remove extra dir, update info

--- crypto-test/info.rkt
+++ crypto-test/info.rkt
@@ -1,4 +1,12 @@
 #lang info
 
-(define collection 'multi)
-(define deps '("base"))
+;; pkg info
+
+(define version "1.0")
+(define collection "crypto")
+(define deps '("base" "rackunit-lib" "asn1-lib" "crypto-lib"))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "crypto")
