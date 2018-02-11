12ec89efa37d3566250c8e57429fb356c7dcc612
Ryan Culpepper <ryanc@racket-lang.org>
2018-02-11 22:59:31 +0100

    crypto-lib: remove extra dir, update info

--- crypto-lib/info.rkt
+++ crypto-lib/info.rkt
@@ -1,5 +1,12 @@
 #lang info
 
-(define collection 'multi)
+;; pkg info
 
-(define deps '("base" "asn1-lib"))
+(define version "1.0")
+(define collection "crypto")
+(define deps '("base" "asn1-lib" "binaryio-lib"))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "crypto")
