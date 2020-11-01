9baf875d4b6b83c2c9735357fad5977c0c710c53
Ryan Culpepper <ryanc@racket-lang.org>
2020-11-01 15:31:19 +0100

    ... add info for x509-lib

---
+++ x509-lib/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+;; pkg info
+
+(define version "1.0")
+(define collection "x509")
+(define deps '("base" "asn1-lib" "crypto-lib"))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "x509")
