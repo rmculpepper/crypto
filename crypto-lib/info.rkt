8a97846cc56eaee54b538f27a8289ea04d54d519
Bogdan Popa <bogdan@defn.io>
2020-12-23 14:03:15 +0200

    pkg: bump version to 1.6

--- crypto-lib/info.rkt
+++ crypto-lib/info.rkt
@@ -2,7 +2,7 @@
 
 ;; pkg info
 
-(define version "1.5")
+(define version "1.6")
 (define collection "crypto")
 (define deps '("base" "asn1-lib" "binaryio-lib" ["gmp-lib" #:version "1.1"]))
 (define pkg-authors '(ryanc))
