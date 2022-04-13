e2e42e2da1301cf4534fb79fc02f684f2d1602c0
Ryan Culpepper <ryanc@racket-lang.org>
2022-04-13 21:56:35 +0200

    crypto: bump version to 1.8

--- crypto-lib/info.rkt
+++ crypto-lib/info.rkt
@@ -2,7 +2,7 @@
 
 ;; pkg info
 
-(define version "1.7")
+(define version "1.8")
 (define collection "crypto")
 (define deps '("base" "asn1-lib" "base64-lib" "binaryio-lib" ["gmp-lib" #:version "1.1"]))
 (define pkg-authors '(ryanc))
